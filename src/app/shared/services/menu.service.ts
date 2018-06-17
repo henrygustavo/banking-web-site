3a578471fc53cde333b836e2d949c8ea61c76b6c
henrygustavo <henrygustavof@gmail.com>
2018-06-17 12:43:14 -0500

    change

--- src/app/shared/services/menu.service.ts
+++ src/app/shared/services/menu.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/observable';
 import { Subject } from 'rxjs/Subject';
-
+import { Observable } from 'rxjs/observable';
 import { MenuItem } from '../models/menu-item';
 
 @Injectable()
